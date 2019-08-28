3e64fac2a219df35846e75d460943f8087f2c285
lisas0609 <smithalex@arcor.de>
2019-08-28 11:44:06 +0200

    2 files modified

--- src/app/listen/listen.component.ts
+++ src/app/listen/listen.component.ts
@@ -8,8 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class ListenComponent implements OnInit {
 
   constructor() { }
-  Variable: number;
+  ende: number;
+  liste: Array<number> = [1, 2];
   ngOnInit() {
   }
+  zaehle() {
+    this.liste = [];
 
+    for (let i = 0; i <= this.ende; i = i + 1)
+      this.liste.push(i);
+  }
 }
